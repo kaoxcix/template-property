b5ff9846ed49c1c7457010ce696e69ebf62f11f6
kaoxcix <kaoxcix@hotmail.com>
2018-01-06 01:32:14 +0700

    add version

--- assets/main.js
+++ assets/main.js
@@ -1,55 +1,22 @@
 $(document)
     .ready(function() {
 
+        // Semantic UI Initializing
         $('.ui.checkbox').checkbox();
         $('.ui.dropdown').dropdown();
 
+        // Glide Initializing
         var hero = $('#hero').glide({
             type: 'carousel',
         });
 
-        var carousel = $('#Carousel2').glide({
-            type: 'carousel',
-        });
-
-        var $root = $('html, body');
-
-        // $('a[href^="#"]').click(function() {
-        //     var href = $.attr(this, 'href');
-        //
-        //     $root.animate({
-        //         scrollTop: $(href).offset().top
-        //     }, 500, function () {
-        //         window.location.hash = href;
-        //     });
-        //
-        //     return false;
-        // });
-
+        // Smooth Scroll
         $('a').click(function(){
             $('html, body').animate({
                 scrollTop: $( $(this).attr('href') ).offset().top
             }, 500);
             return false;
         });
-
-        // // fix menu when passed
-        // $('.masthead')
-        //     .visibility({
-        //         once: false,
-        //         onBottomPassed: function() {
-        //             $('.fixed.menu').transition('fade in');
-        //         },
-        //         onBottomPassedReverse: function() {
-        //             $('.fixed.menu').transition('fade out');
-        //         }
-        //     })
-        // ;
-        //
-        // // create sidebar and attach to menu open
-        // $('.ui.sidebar')
-        //     .sidebar('attach events', '.toc.item')
-        // ;
 
     })
 ;
