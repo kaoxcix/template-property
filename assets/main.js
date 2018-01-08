c44155b14ffe13ff7f15157ba10ad9e00128c103
kaoxcix <kaoxcix@hotmail.com>
2018-01-09 00:43:33 +0700

    add menu

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
 $(document)
-    .ready(function() {
+    .ready(function () {
 
         // Semantic UI Initializing
         $('.ui.checkbox').checkbox();
@@ -10,13 +10,33 @@ $(document)
             type: 'carousel',
         });
 
-        // Smooth Scroll
-        $('a').click(function(){
+        // Smooth Scroll for all Link
+        $('a.smooth-scroll').click(function (e) {
+            e.preventDefault();
             $('html, body').animate({
-                scrollTop: $( $(this).attr('href') ).offset().top
+                scrollTop: $($(this).attr('href')).offset().top
             }, 500);
             return false;
         });
 
+        // Smooth Scroll to Top
+        $(window).scroll(function() {
+            if ($(this).scrollTop()) {
+                $('#go-to-top').fadeIn();
+            } else {
+                $('#go-to-top').fadeOut();
+            }
+        });
+
+        $("#go-to-top").click(function (e) {
+            e.preventDefault();
+            $("html, body").animate({scrollTop: 0}, 500);
+        });
+
+        // Toggle Hamburger Main Menu
+        $('#hamburger').click(function (e) {
+            e.preventDefault();
+            $('#main-menu').toggle();
+        });
     })
 ;
